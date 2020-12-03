e50b9347ebf42ba267ea74a887a6415a016f45bd
Dave Powell <evadllewop@gmail.com>
2020-12-03 14:46:29 -0600

    updated index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,68 +1,81 @@
 
+//create variables for all of the 'requires'
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+// const util = require('util');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-const writeFileAsync = util.promisify(fs.writeFile);
+const path = require('path')
 
 // array of questions for user
-const questions = () =>
-    inquirer.prompt([
-        {
+const questions = [
+    {
 
-            type: `input`,
-            name: `title`,
-            message: `What is the title of your project?`,
-            trim: function (answer) {
-                this.question.trim();
-            },
-
-            type: `input`,
-            name: `description`,
-            message: `Please provide a short description of your project.`,
-            trim: function (answer) {
-                this.question.trim();
-            },
-
-            type: `input`,
-            name: `install`,
-            message: `What are the steps required to install your project?`,
-            trim: function (answer) {
-                this.question.trim();
-            },
-
-            type: `input`,
-            name: `usage`,
-            message: `What are the steps required to install your project?`,
-            trim: function (answer) {
-                this.question.trim();
-            },
-
-            type: 'list',
-            name: 'license',
-            message: `What liscense will you be using for your project?`,
-            choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal'],
-
+        type: `input`,
+        name: `title`,
+        message: `What is the title of your project?`,
+        trim: function (answer) {
+            this.question.trim();
         },
+    }, {
+        type: `input`,
+        name: `description`,
+        message: `Please provide a short description of your project.`,
+        trim: function (answer) {
+            this.question.trim();
+        },
+    }, {
+        type: `input`,
+        name: `install`,
+        message: `What are the steps required to install your project?`,
+        trim: function (answer) {
+            this.question.trim();
+        },
+    }, {
+        type: `input`,
+        name: `usage`,
+        message: `What are the steps required to install your project?`,
+        trim: function (answer) {
+            this.question.trim();
+        },
+    }, {
+        type: 'list',
+        name: 'license',
+        message: `What liscense will you be using for your project?`,
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal'],
 
-    ]);
+    }
+
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
+    return fs.writeFile(path.join(process.cwd(), fileName), data, err => {
         if (err) {
-            return console.log(err);
+            console.log(err);
         }
 
-        console.log("Your README.md file has been generated successfully");
+        console.log("Your README.md file has been generated successfully")
     });
 }
 
 // function to initialize program
 function init() {
-    console.log("hello");
+    // console.log("hello");
+    inquirer.prompt(questions)
 
-};
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log(answers)
+            writeToFile('README.md', generateMarkdown(answers))
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else when wrong
+            }
+        });
+}
 
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,24 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * 
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Liscense
+  ${data.license}
 
 `;
 }
